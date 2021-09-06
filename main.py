a78ff433409480c3cd6d7e4f5eab74b1c614ddf7
ViggoMode210 <86791607+ViggoMode2021@users.noreply.github.com>
2021-09-06 18:03:54 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
+
+subscribers = []
 
 @app.route('/')
 def index():
@@ -9,18 +11,25 @@ def index():
 
 @app.route('/about')
 def about():
+    title = 'Sobre Ryan Viglione'
     names = ['Vida', 'Trabajo', 'Pasatiempos', 'Enlaces']
-    return render_template("about.html", names = names)
+    return render_template("about.html", names = names, title= title)
+
+@app.route('/subscribe')
+def subscribe():
+    title_two = 'Puedes subscribirte ahora.'
+    return render_template('subscribe.html', title=title_two)
+
+@app.route('/form', methods = ["POST"])
+def form():
+    first_name = request.form.get("first_name")
+    last_name = request.form.get("last_name")
+    email = request.form.get("email")
+    subscribers.append(f"{first_name} {last_name} {email}")
+    title_three = 'Muchas gracias.'
+    return render_template('form.html', title=title_three, subscribers=subscribers)
 
 if __name__ == "__main__":
     app.run(debug=True)
 
-
-'''
-
-   {% if title %}
-        {{ title }}
-      {% else %}
-        Ryan Viglione
-        {{ endif }}
-'''
+#first_name = first_name, last_name= last_name, email = email

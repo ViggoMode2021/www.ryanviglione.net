439cde99d097713f01cf0f52f2a7308bf8cae9bf
ViggoMode210 <86791607+ViggoMode2021@users.noreply.github.com>
2021-12-04 17:17:05 -0500

    Add files via upload

---
+++ music-list.js
@@ -0,0 +1,46 @@
+
+let allMusic = [
+  {
+    name: "Vig Town Road",
+    artist: "Señor Vig",
+    img: "music-1",
+    src: "Vig"
+  },
+  {
+    name: "Capitales",
+    artist: "Señor Vig",
+    img: "preposiciones",
+    src: "capitales"
+  },
+  {
+    name: "Reflexivo",
+    artist: "Señor Vig",
+    img: "music-3",
+    src: "reflexive"
+  },
+  {
+    name: "Hay que/Se prohíbe",
+    artist: "Señor Vig",
+    img: "music-4",
+    src: "hay"
+  },
+  {
+    name: "Centro comercial",
+    artist: "Señor Vig",
+    img: "music-5",
+    src: "ropa"
+  },
+  {
+    name: "Preposiciones",
+    artist: "Señor Vig",
+    img: "music-6",
+    src: "prepositions"
+  },
+  // like this paste it and remember to give comma after ending of this bracket }
+  // {
+  //   name: "Here is the music name",
+  //   artist: "Here is the artist name",
+  //   img: "image name here - remember img must be in .jpg formate and it's inside the images folder of this project folder",
+  //   src: "music name here - remember img must be in .mp3 formate and it's inside the songs folder of this project folder"
+  // }
+];

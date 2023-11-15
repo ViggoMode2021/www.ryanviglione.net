827a367375468ba9123441b1119502c268f554c5
Ryan Viglione <86791607+ViggoMode2021@users.noreply.github.com>
2023-11-14 20:21:40 -0500

    Add files via upload

--- static/js/terminal.js
+++ static/js/terminal.js
@@ -12,10 +12,32 @@ document.addEventListener('DOMContentLoaded', function () {
         const command = terminalInput.value;
         appendOutput(`<div><span>&gt;</span> ${command}</div>`);
 
-        // Handle the command - you can add your logic here
+        // Execute the command
+        executeCommand(command);
 
         // Clear input
         terminalInput.value = '';
+    }
+
+    function executeCommand(command) {
+        let output = '';
+
+        // Basic commands
+        if (command.toLowerCase() === 'help') {
+            output = 'Available commands: show websites, visit';
+        } else if (command.toLowerCase() === 'clear') {
+            terminalOutput.innerHTML = '';
+            return;
+        } else if (command.toLowerCase() === 'visit linkedin') {
+            window.location.href = 'https://www.linkedin.com/in/ryan-viglione/';
+        } else if (command.toLowerCase() === 'show websites') {
+            output = "LinkedIn GitHub VigLMS PowerShell Projects EliteShell";
+        } else if (command.toLowerCase() === 'visit GitHub') {
+            window.location.href = 'https://www.github.com/viggomode2021';
+        } else {
+            output = `Command not found: ${command}`;
+        }
+        appendOutput(`<div>${output}</div>`);
     }
 
     function appendOutput(content) {
